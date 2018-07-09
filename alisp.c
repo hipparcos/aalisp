0da6ac93063758ce50ae8cb675f343554195566d
Adrien Aucher <aucher.adrien@gmail.com>
2018-07-09 18:43:41 +0100

    alisp: add simple read loop with configurable prompt.

--- alisp.c
+++ alisp.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "version.h"
 
-#define MACRO_TO_STR(s) #s
-#define STR(s) MACRO_TO_STR(s)
+#define INPUT_SIZE  2048
+
+static char input[INPUT_SIZE];
+static char* prompt = "> ";
 
 int main(int argc, char** argv) {
+    /* Command line arguments */
+    int c;
+    while ((c = getopt(argc, argv, "p:")) != -1) {
+        switch (c) {
+            case 'p':
+                prompt = optarg;
+                break;
+        }
+    }
+
     /* Print infos */
-    puts(PROGNAME" "VERSION"-"CODENAME" build "STR(BUILD));
+    printf(PROGNAME" "VERSION"-"CODENAME" build %d\n", BUILD);
     puts("Press Ctrl+C to exit.\n");
+
+    /* REPL loop */
+    while (true) {
+        fputs(prompt, stdout);
+
+        fgets(input, INPUT_SIZE, stdin);
+
+        fputs(input, stdout);
+    }
 
     return EXIT_SUCCESS;
 }
--- buildnumber.mk
+++ buildnumber.mk
@@ -1,2 +1,2 @@
 # Automatically incremented.
-BUILDNUMBER=3
+BUILDNUMBER=9

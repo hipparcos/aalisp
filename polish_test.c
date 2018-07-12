ce63c51d3023be37e83268b6af39c626ad5b2c1a
Adrien Aucher <aucher.adrien@gmail.com>
2018-07-12 15:27:37 +0100

    polish: add tests for polish parser.

--- polish_test.c
+++ polish_test.c
@@ -10,24 +10,26 @@ int tests_run = 0;
 struct testcase {
     struct lval x;
     struct lval y;
+    char* input;
     struct lval expected;
 };
 
 static char* test_op_helper(
-        const char* format,
-        struct lval (*op)(struct lval, struct lval),
-        struct testcase testcases[],
-        unsigned long number_of_cases) {
+    const char* format,
+    struct lval (*op)(struct lval, struct lval),
+    struct testcase testcases[],
+    unsigned long number_of_cases) {
+
     char* message = (char*)calloc(sizeof(char), 1024);
     struct lval got;
+    char temp[4][128];
 
     for (int i = 0; i < (int)number_of_cases; i++) {
         struct testcase tt = testcases[i];
         got = op(tt.x, tt.y);
-        char temp[4][128];
         lval_to_string(tt.x, temp[0]);
         lval_to_string(tt.y, temp[1]);
-        lval_to_string(tt.expected, temp[2]),
+        lval_to_string(tt.expected, temp[2]);
         lval_to_string(got, temp[3]);
         sprintf(message, format, temp[0], temp[1], temp[2], temp[3]);
         mu_assert(message, lval_equals(got, tt.expected) || lval_err_equals(got, tt.expected));
@@ -46,7 +48,7 @@ static char* test_polish_op_add() {
         {.x= lval_dbl(1.1), .y= lval_dbl(1.1), .expected= lval_dbl(2.2)},
     };
     return test_op_helper("op_add %s + %s = %s got %s", polish_op_add,
-            testcases, LENGTH(testcases));
+                          testcases, LENGTH(testcases));
 }
 
 static char* test_polish_op_sub() {
@@ -56,7 +58,7 @@ static char* test_polish_op_sub() {
         {.x= lval_dbl(1.1), .y= lval_dbl(1.1), .expected= lval_dbl(0.0)},
     };
     return test_op_helper("op_sub %s - %s = %s got %s", polish_op_sub,
-            testcases, LENGTH(testcases));
+                          testcases, LENGTH(testcases));
 }
 
 static char* test_polish_op_mul() {
@@ -66,7 +68,7 @@ static char* test_polish_op_mul() {
         {.x= lval_dbl(2.0), .y= lval_dbl(1.1), .expected= lval_dbl(2.2)},
     };
     return test_op_helper("op_mul %s * %s = %s got %s", polish_op_mul,
-            testcases, LENGTH(testcases));
+                          testcases, LENGTH(testcases));
 }
 
 static char* test_polish_op_div() {
@@ -78,7 +80,7 @@ static char* test_polish_op_div() {
         {.x= lval_num(2), .y= lval_dbl(.0), .expected= lval_err(LERR_DIV_ZERO)},
     };
     return test_op_helper("op_div %s / %s = %s got %s", polish_op_div,
-            testcases, LENGTH(testcases));
+                          testcases, LENGTH(testcases));
 }
 
 static char* test_polish_op_mod() {
@@ -89,7 +91,44 @@ static char* test_polish_op_mod() {
         {.x= lval_dbl(2.0), .y= lval_dbl(2.0), .expected= lval_err(LERR_BAD_NUM)},
     };
     return test_op_helper("op_mod %s %% %s = %s got %s", polish_op_mod,
-            testcases, LENGTH(testcases));
+                          testcases, LENGTH(testcases));
+}
+
+static char* test_polish_lang() {
+    struct testcase testcases[] = {
+        {.input= "+ 1 1", .expected= lval_num(2)},
+        {.input= "+ 1 (* 10 2)", .expected= lval_num(21)},
+        {.input= "+ 1.0 (* 10 2)", .expected= lval_dbl(21)},
+        {.input= "/ 10 0", .expected= lval_err(LERR_DIV_ZERO)},
+    };
+
+    char* message = (char*)calloc(sizeof(char), 1024);
+    char temp[2][128];
+
+    polish_setup();
+
+    for (int i = 0; i < (int)(LENGTH(testcases)); i++) {
+        struct testcase tt = testcases[i];
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", tt.input, Polish, &r)) {
+            struct lval got = polish_eval_expr(r.output);
+            lval_to_string(tt.expected, temp[0]);
+            lval_to_string(got, temp[1]);
+            mpc_ast_delete(r.output);
+            sprintf(message, "polish_lang: fail for `%s` = %s got %s", tt.input, temp[0], temp[1]);
+            mu_assert(message, lval_equals(got, tt.expected) || lval_err_equals(got, tt.expected));
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+            sprintf(message, "polish_lang: fail for `%s`", tt.input);
+            mu_assert(message, false);
+        }
+    }
+
+    polish_cleanup();
+
+    free(message); // Avoid clang complaint.
+    return 0;
 }
 
 static char* all_tests() {
@@ -98,6 +137,7 @@ static char* all_tests() {
     mu_run_test(test_polish_op_mul);
     mu_run_test(test_polish_op_div);
     mu_run_test(test_polish_op_mod);
+    mu_run_test(test_polish_lang);
     return 0;
 }
 

171b44421b7117c9df5860cd7cbe4efef94ebc2a
Adrien Aucher <aucher.adrien@gmail.com>
2018-07-17 18:45:53 +0100

    make: refactor test.mk to work with snow.

--- test.mk
+++ test.mk
@@ -1,22 +1,29 @@
 tests:=lval_test.c #lbuiltin_test.c
 test_build_dir:=$(build_dir)
 
-testcases:=$(addprefix $(test_build_dir)/,$(tests:%.c=%))
-testobjects:=$(addprefix $(test_build_dir)/,$(tests:%.c=%.o))
+testobjects:=$(tests:%.c=%.o)
+testobjects_built:=$(addprefix $(test_build_dir)/,$(testobjects))
+testcases:=$(tests:%.c=%)
+testcases_built:=$(addprefix $(test_build_dir)/,$(testcases))
 
 TEST_CFLAGS:=-DSNOW_ENABLED -g
 
 test: $(testcases)
 
-# $(testcases): % : %.o $(filter-out $(build_dir)/$(PROGNAME).o,$(objects))
-build/lval_test: $(addprefix $(build_dir)/,lval_test.o lval.o vendor/mini-gmp/mini-gmp.o)
-	@$(CC) $(CFLAGS) $(TEST_CFLAGS) $^ $(LDFLAGS) -o $@
+$(testcases): %: $(test_build_dir)/%
+
+$(testcases_built): % : %.o $(filter-out $(build_dir)/$(PROGNAME).o,$(objects))
+# $(test_build_dir)/%_test: $(test_build_dir)/%_test.o $(filter-out $(build_dir)/$(PROGNAME).o,$(objects))
+	@$(CC) $(LDFLAGS) $(LDLIBS) $^ $(TEST_CFLAGS) -o $@
 	@valgrind $(VGFLAGS) ./$@
 
+$(test_build_dir)/%_test.o: %_test.c $$(@D)/.f
+	@$(CC) $(CFLAGS) $(TEST_CFLAGS) -c -o $@ $<
+
 clean::
-	rm -f $(testobjects) $(testcases)
+	rm -f $(testobjects_built) $(testcases_built)
 
 # Include dependancies makefiles.
 include $(tests:%.c=$(build_dir)/%.d)
 
-.PHONY: test $(testcases)
+.PHONY: test $(testcases) $(testcases_built)

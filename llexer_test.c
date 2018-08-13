13e6f0204ade46fdce1e3f3455009c0cb3f0148b
Adrien Aucher <aucher.adrien@gmail.com>
2018-08-13 14:50:00 +0100

    llexer: refactor test file.
    Add a function which builds a token list.

--- llexer_test.c
+++ llexer_test.c
@@ -1,151 +1,186 @@
 #include "llexer.h"
 
+#include <string.h>
+
 #include "vendor/snow/snow/snow.h"
 
-#define test_helper(input, expected) _test_helper(input, expected, lisp_lex)
-#define test_helper_surround(input, expected) _test_helper(input, expected, lisp_lex_surround)
+struct token_list {
+    enum ltok_type type;
+    char* content;
+};
 
-bool _test_helper(const char* input, struct ltok* expected, struct ltok* (*lexer)(const char*, struct ltok**)) {
-    struct ltok *first = NULL, *error = NULL;
-    first = lexer(input, &error);
-    bool ret = false;
-    if (error != NULL || !(ret = llex_are_all_equal(first, expected))) {
-        fputs("\nInput:", stdout);
-        fputs(input, stdout);
-        fputc('\n', stdout);
-        fputs("Got:\n", stdout);
-        llex_print_all(first);
-        fputs("Expected:\n", stdout);
-        llex_print_all(expected);
-    }
-    llex_free(first);
-    return ret;
+/** token_list_builder builds singly linked list of tokens from list. */
+struct ltok* token_list_builder(const struct token_list* list) {
+    struct ltok *head = NULL, **last = &head;
+    do {
+        struct ltok* curr = calloc(1, sizeof(struct ltok));
+        curr->type = list->type;
+        size_t len = strlen(list->content);
+        curr->content = calloc(1, len + 1);
+        strncpy(curr->content, list->content, len);
+        *last = curr;
+        last = &(curr->next);
+    } while (++list && list->content != NULL);
+    return head;
 }
+
+/** test_pass displays input in header (lisp_lex lexer func). */
+#define test_pass(msg, input, expected) \
+    it("passes for " msg " `" input "`", { \
+        _test_pass_body(input, expected, lisp_lex) \
+    })
+
+/** test_pass displays input in header (lisp_lex_surround lexer func). */
+#define test_pass_surround(msg, input, expected) \
+    it("passes for " msg " `(" input ")` (surrounded input)", { \
+        _test_pass_body(input, expected, lisp_lex_surround) \
+    })
+
+/** test_pass does not display input in header (lisp_lex lexer func). */
+#define test_pass_raw(msg, input, expected) \
+    it("passes for " msg, { \
+        _test_pass_body(input, expected, lisp_lex) \
+    })
+
+#define _test_pass_body(input, expected, lexer) \
+        struct ltok* expec = token_list_builder(expected); \
+        defer(llex_free(expec)); \
+        struct ltok *got = NULL, *error = NULL; \
+        defer(llex_free(got)); \
+        assert(got = lexer(input, &error)); \
+        assert(error == NULL); \
+        assert(llex_are_all_equal(got, expec));
+
+#define test_fail(msg, input) \
+    it("fails for " msg " `" input "`", { \
+        struct ltok *got = NULL, *error = NULL; \
+        assert(got = lisp_lex(input, &error)); \
+        defer(llex_free(got)); \
+        assert(error != NULL); \
+    })
 
 describe(llex, {
 
-    it("passes for zero length string", {
-        const char* input = "";
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        struct ltok* expected = &tEOF;
-        assert(test_helper(input, expected));
-    });
+    test_pass_raw("NULL",
+            NULL,
+            &((struct token_list[]){
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("fails for unknwon characters", {
-        const char* input = "[]";
-        struct ltok *first = NULL, *error = NULL;
-        first = lisp_lex(input, &error);
-        defer(llex_free(first));
-        assert(error != NULL);
-    });
+    test_pass("0-length",
+            "",
+            &((struct token_list[]){
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("skip spaces", {
-        const char* input = "   \t \r \n   ";
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        struct ltok* expected = &tEOF;
-        assert(test_helper(input, expected));
-    });
+    test_pass_raw("spaces",
+            "   \t \r \n   ",
+            &((struct token_list[]){
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for parenthesis", {
-        const char* input = "(()";
-        struct ltok tok1 = {.type= LTOK_OPAR, .content= "("};
-        struct ltok tok2 = {.type= LTOK_OPAR, .content= "("};
-        struct ltok tok3 = {.type= LTOK_CPAR, .content= ")"};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tok3;
-        tok3.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("parenthesis",
+            "(()",
+            &((struct token_list[]){
+                {LTOK_OPAR, "("},
+                {LTOK_OPAR, "("},
+                {LTOK_CPAR, ")"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for integers", {
-        const char* input = "1 234  -32";
-        struct ltok tok1 = {.type= LTOK_NUM, .content= "1"};
-        struct ltok tok2 = {.type= LTOK_NUM, .content= "234"};
-        struct ltok tok3 = {.type= LTOK_NUM, .content= "-32"};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tok3;
-        tok3.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("integers",
+            "1 234  -32",
+            &((struct token_list[]){
+                {.type= LTOK_NUM, .content= "1"},
+                {.type= LTOK_NUM, .content= "234"},
+                {.type= LTOK_NUM, .content= "-32"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for doubles", {
-        const char* input = "0.0 234.0 -32.0";
-        struct ltok tok1 = {.type= LTOK_DBL, .content= "0.0"};
-        struct ltok tok2 = {.type= LTOK_DBL, .content= "234.0"};
-        struct ltok tok3 = {.type= LTOK_DBL, .content= "-32.0"};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tok3;
-        tok3.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("doubles",
+            "0.0 234.0  -32.0",
+            &((struct token_list[]){
+                {LTOK_DBL, "0.0"},
+                {LTOK_DBL, "234.0"},
+                {LTOK_DBL, "-32.0"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for strings", {
-        const char* input = " \"string1\"  \"string2\"";
-        struct ltok tok1 = {.type= LTOK_STR, .content= "\"string1\""};
-        struct ltok tok2 = {.type= LTOK_STR, .content= "\"string2\""};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("strings",
+            "\"string1\"  \"string2\"",
+            &((struct token_list[]){
+                {LTOK_STR, "\"string1\""},
+                {LTOK_STR, "\"string2\""},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for escaped strings", {
-        const char* input = " \"string1\\\"escape\"  \"string2 \\\" with escape and space and number 42.\"";
-        struct ltok tok1 = {.type= LTOK_STR, .content= "\"string1\\\"escape\""};
-        struct ltok tok2 = {.type= LTOK_STR, .content= "\"string2 \\\" with escape and space and number 42.\""};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("escaped strings",
+            "\"string1\\\"escape\"",
+            &((struct token_list[]){
+                {LTOK_STR, "\"string1\\\"escape\""},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("fails for unclosed strings", {
-        const char* input = "    \"string not closed";
-        struct ltok *first = NULL, *error = NULL;
-        first = lisp_lex(input, &error);
-        defer(llex_free(first));
-        assert(error != NULL);
-    });
+    test_pass("symbols",
+            "symbol  + == $",
+            &((struct token_list[]){
+                {LTOK_SYM, "symbol"},
+                {LTOK_SYM, "+"},
+                {LTOK_SYM, "=="},
+                {LTOK_SYM, "$"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("passes for symbols", {
-        const char* input = " symbol  + == $";
-        struct ltok tok1 = {.type= LTOK_SYM, .content="symbol"};
-        struct ltok tok2 = {.type= LTOK_SYM, .content="+"};
-        struct ltok tok3 = {.type= LTOK_SYM, .content="=="};
-        struct ltok tok4 = {.type= LTOK_SYM, .content="$"};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok1.next = &tok2;
-        tok2.next = &tok3;
-        tok3.next = &tok4;
-        tok4.next = &tEOF;
-        struct ltok* expected = &tok1;
-        assert(test_helper(input, expected));
-    });
+    test_pass("simple expression",
+            "+ 1 (! 21)",
+            &((struct token_list[]){
+                {LTOK_SYM, "+"},
+                {LTOK_NUM, "1"},
+                {LTOK_OPAR, "("},
+                {LTOK_SYM, "!"},
+                {LTOK_NUM, "21"},
+                {LTOK_CPAR, ")"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
 
-    it("surrounds the output with `(` & `)`", {
-        const char* input = "+ 1 2";
-        struct ltok tok0 = {.type= LTOK_OPAR, .content= "("};
-        struct ltok tok1 = {.type= LTOK_SYM, .content= "+"};
-        struct ltok tok2 = {.type= LTOK_NUM, .content= "1"};
-        struct ltok tok3 = {.type= LTOK_NUM, .content= "2"};
-        struct ltok tok4 = {.type= LTOK_CPAR, .content= ")"};
-        struct ltok tEOF = {.type= LTOK_EOF, .content= NULL};
-        tok0.next = &tok1;
-        tok1.next = &tok2;
-        tok2.next = &tok3;
-        tok3.next = &tok4;
-        tok4.next = &tEOF;
-        struct ltok* expected = &tok0;
-        assert(test_helper_surround(input, expected));
-    });
+    test_pass_surround("simple expression",
+            "+ 1 (! 21)",
+            &((struct token_list[]){
+                {LTOK_OPAR, "("},
+                {LTOK_SYM, "+"},
+                {LTOK_NUM, "1"},
+                {LTOK_OPAR, "("},
+                {LTOK_SYM, "!"},
+                {LTOK_NUM, "21"},
+                {LTOK_CPAR, ")"},
+                {LTOK_CPAR, ")"},
+                {LTOK_EOF, ""},
+                {0, NULL}
+            })[0]
+        );
+
+    test_fail("unknown character", "[]");
+    test_fail("unclosed strings", "\"string not closed");
 
 });
 

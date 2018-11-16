f37d70899f27c3ed9d7de23dbf9506a852189969
Adrien Aucher <aucher.adrien@gmail.com>
2018-11-16 18:41:01 +0000

    llexer: refactor llexer interface.
    Changes:
    - Add config macros;
    - Add ltok.len field;
    - Rename lisp_lex to llex_lex;
    - Remove lisp_lex_surround;
    - Add documentation;
    - Fix typos.

--- llexer.h
+++ llexer.h
@@ -1,5 +1,5 @@
-#ifndef _H_LLEXER_
-#define _H_LLEXER_
+#ifndef H_LLEXER_
+#define H_LLEXER_
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -7,49 +7,67 @@
 
 #include "lerr.h"
 
+#define LLEX_COMM ';' // is the comment mark.
+#define LLEX_OPAR '(' // is the opening parenthesis.
+#define LLEX_CPAR ')' // is the closing parenthesis.
+#define LLEX_OBRC '{' // is the opening brace.
+#define LLEX_CBRC '}' // is the closing brace.
+#define LLEX_OSTR '"' // is the opening quote.
+#define LLEX_CSTR '"' // is the closing quote.
+#define LLEX_DOLL '$' // is the special $ character.
+#define LLEX_SYMB "+-*/%^?!&|:,._#~<>=$§£¤µ\\" // is a list of characters allowed in symbols.
+
+/** ltok_type is the type of a dialecte token. */
 enum ltok_type {
-    LTOK_EOF= 0,
-    LTOK_ERR,
-    LTOK_OPAR,
-    LTOK_CPAR,
-    LTOK_OBRC,
-    LTOK_CBRC,
-    LTOK_DOLL,
-    LTOK_SYM,
-    LTOK_NUM,
-    LTOK_DBL,
-    LTOK_STR,
+    LTOK_EOF = 0, // is a terminal token, nothing afterwards.
+    LTOK_ERR,     // signals the encounter of an unknown character.
+    LTOK_OPAR,    // is an opening parenthesis.
+    LTOK_CPAR,    // is a closing parenthesis.
+    LTOK_OBRC,    // is an opening brace.
+    LTOK_CBRC,    // is a closing brace.
+    LTOK_DOLL,    // is the special $ syntax.
+    LTOK_SYM,     // is a symbol.
+    LTOK_NUM,     // is an integral number.
+    LTOK_DBL,     // is a double number.
+    LTOK_STR,     // is a string.
 };
 
+/** ltok is dialecte token or a list of dialecte tokens. */
 struct ltok {
+    /** ltok.type is the type of the token. */
     enum ltok_type type;
+    /** ltok.content is the value of the token. */
     char* content;
+    /** ltok.len is the length of ltok.content. */
+    size_t len;
+    /** ltok.line is the line where this token starts. */
     int   line;
+    /** ltok.col is the column where this token starts. */
     int   col;
-    /* singly linked list */
+    /** ltok.next is the next element of the list (singly linked list). */
+    /** ltok.next is NULL for LTOK_EOF tokens. */
     struct ltok* next;
 };
 
-/** lisp_lex transforms the input into a list of tokens.
+/** llex_lex transforms the input into a list of tokens.
  ** Returns the first element of the list of ltok.
  ** The returned list always end with a LTOK_EOF token.
  ** err is allocated in case of error.
- ** Caller is responsible for calling llex_free() on tokens. */
-struct ltok* lisp_lex(const char* input, struct lerr** err);
-/** lisp_lex_surround acts as lisp_lex but surrounds tokens with `(` & `)`.
- ** It ensures that the output is a sexpr.  */
-struct ltok* lisp_lex_surround(const char* input, struct lerr** err);
+ ** Caller is responsible for calling llex_free() on the returned list of tokens. */
+struct ltok* llex_lex(const char* input, struct lerr** err);
+/** llex_lex_from calls llex_lex with the content of the file input. */
+struct ltok* llex_lex_from(const FILE* input, struct lerr** err);
 /** llex_free clears a list of tokens.
- ** The list of tokens must end with a token of type LTOK_EOS.
+ ** The list of tokens must end with a token of type LTOK_EOF.
  ** tokens must not be used afterwards */
 void llex_free(struct ltok* tokens);
 
 /** llex_are_equal tells if two tokens are equal. */
 bool llex_are_equal(struct ltok* left, struct ltok* right);
-/** llex_are_all_equal tells if two list of tokens are equal. */
+/** llex_are_all_equal tells if two lists of tokens are equal. */
 bool llex_are_all_equal(struct ltok* left, struct ltok* right);
 
-/** llex_ty_string returns the type of a token as string. */
+/** llex_type_string returns the type of a token as string. */
 const char* llex_type_string(enum ltok_type type);
 /** llex_print_to prints token to the out file. */
 void llex_print_to(struct ltok* token, FILE* out);

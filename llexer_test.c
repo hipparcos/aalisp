7ac18dbd391102833be9c59ea882847d7968a99f
Adrien Aucher <aucher.adrien@gmail.com>
2018-11-16 19:32:49 +0000

    llexer: change lisp_lex() for llex_lex() in tests.

--- llexer_test.c
+++ llexer_test.c
@@ -24,32 +24,26 @@ struct ltok* token_list_builder(const struct token_list* list) {
     return head;
 }
 
-/** test_pass displays input in header (lisp_lex lexer func). */
+/** test_pass displays input in header. */
 #define test_pass(msg, input, expected) \
     it("passes for " msg " `" input "`", { \
-        _test_pass_body(input, expected, lisp_lex) \
+        _test_pass_body(input, expected) \
     })
 
-/** test_pass displays input in header (lisp_lex_surround lexer func). */
-#define test_pass_surround(msg, input, expected) \
-    it("passes for " msg " `" input "` (surrounded input)", { \
-        _test_pass_body(input, expected, lisp_lex_surround) \
-    })
-
-/** test_pass does not display input in header (lisp_lex lexer func). */
+/** test_pass_raw does not display input in header. */
 #define test_pass_raw(msg, input, expected) \
     it("passes for " msg, { \
-        _test_pass_body(input, expected, lisp_lex) \
+        _test_pass_body(input, expected) \
     })
 
-#define _test_pass_body(input, expected, lexer) \
+#define _test_pass_body(input, expected) \
         struct ltok* expec = token_list_builder(expected); \
         defer(llex_free(expec)); \
         struct lerr* err = NULL; \
         defer(lerr_free(err)); \
         struct ltok* got = NULL; \
         defer(llex_free(got)); \
-        assert(got = lexer(input, &err)); \
+        assert(got = llex_lex(input, &err)); \
         assert(err == NULL); \
         assert(llex_are_all_equal(got, expec));
 
@@ -58,7 +52,7 @@ struct ltok* token_list_builder(const struct token_list* list) {
         struct lerr* err = NULL; \
         defer(lerr_free(err)); \
         struct ltok* got = NULL; \
-        assert(got = lisp_lex(input, &err)); \
+        assert(got = llex_lex(input, &err)); \
         defer(llex_free(got)); \
         assert(err != NULL); \
     })
@@ -207,39 +201,6 @@ describe(llex, {
                 {LTOK_OPAR, "("},
                 {LTOK_SYM, "!"},
                 {LTOK_NUM, "21"},
-                {LTOK_CPAR, ")"},
-                {LTOK_EOF, ""},
-                {0, NULL}
-            })[0]
-        );
-
-    test_pass_surround("simple expression",
-            "+ 1 (! 21)",
-            &((struct token_list[]){
-                {LTOK_OPAR, "("},
-                {LTOK_SYM, "+"},
-                {LTOK_NUM, "1"},
-                {LTOK_OPAR, "("},
-                {LTOK_SYM, "!"},
-                {LTOK_NUM, "21"},
-                {LTOK_CPAR, ")"},
-                {LTOK_CPAR, ")"},
-                {LTOK_EOF, ""},
-                {0, NULL}
-            })[0]
-        );
-
-    test_pass_surround("expr starting with S-Expr",
-            "(head {+}) 1",
-            &((struct token_list[]){
-                {LTOK_OPAR, "("},
-                {LTOK_OPAR, "("},
-                {LTOK_SYM, "head"},
-                {LTOK_OBRC, "{"},
-                {LTOK_SYM, "+"},
-                {LTOK_CBRC, "}"},
-                {LTOK_CPAR, ")"},
-                {LTOK_NUM, "1"},
                 {LTOK_CPAR, ")"},
                 {LTOK_EOF, ""},
                 {0, NULL}

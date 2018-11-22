73e159b7d0b2e96980cba3679125a4cb4caf8d0d
Adrien Aucher <aucher.adrien@gmail.com>
2018-11-22 16:44:57 +0000

    test: make lmutator_test runnable with minimal deps.

--- test.mk
+++ test.mk
@@ -1,6 +1,6 @@
 # Config: files & dirs.
 tests:=generic/avl_test.c generic/mempool_test.c \
-	llexer_test.c lparser_test.c lmut_test.c \
+	llexer_test.c lparser_test.c lmutator_test.c \
 	lval_test.c lenv_test.c lbuiltin_operator_test.c lbuiltin_func_test.c \
 	leval_test.c marker_test.c
 test_build_dir:=$(build_dir)
@@ -21,9 +21,15 @@ test: $(testcases) $(tests_lisp)
 
 llexer_test: $(addprefix $(build_dir)/,llexer.o lerr.o)
 lparser_test: $(addprefix $(build_dir)/,lparser.o llexer.o lerr.o)
+lmutator_test: $(addprefix $(build_dir)/, \
+	lmutator.o lparser.o llexer.o lerr.o \
+	lval.o lfunc.o lenv.o leval.o \
+	lbuiltin.o lbuiltin_condition.o lbuiltin_func.o lbuiltin_operator.o \
+	generic/avl.o \
+	vendor/mini-gmp/mini-gmp.o \
+	)
 
 $(testcases): %: $(test_build_dir)/%.o
-#$(testcases_built): % : %.o #$(filter-out $(build_dir)/$(PROGNAME).o,$(objects))
 	@$(CC) $(LDFLAGS) $(LDLIBS) $^ $(TEST_CFLAGS) -o $(test_build_dir)/$@
 	@if [ $$($(DO_MEMCHECK); echo $$?) -eq 0 ]; then valgrind $(VGFLAGS) ./$(test_build_dir)/$@; \
 		else ./$(test_build_dir)/$@; fi
